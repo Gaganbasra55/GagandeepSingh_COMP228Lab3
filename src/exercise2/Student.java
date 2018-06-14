ddc25d1a57b135233a87732e23fa04710c94351f
Gaganbasra55 <38173348+Gaganbasra55@users.noreply.github.com>
2018-06-14 19:05:22 -0400

    constructor

--- src/exercise2/Student.java
+++ src/exercise2/Student.java
@@ -1,5 +1,16 @@
 package exercise2;
 
-public class Student {
+public abstract class Student {
 
+    //student name
+    private final String name;
+
+    //full time part time status 
+    private boolean status;
+
+    //Constructor
+    public Student(String name, boolean status) {
+        this.name = name;
+        this.status = status;
+    }
 }
