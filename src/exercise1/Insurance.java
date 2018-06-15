ae07e630f26e9de5ac15009c2a4ad2921763ac97
Gaganbasra55 <38173348+Gaganbasra55@users.noreply.github.com>
2018-06-15 13:55:53 -0400

    insurance class created

---
+++ src/exercise1/Insurance.java
@@ -0,0 +1,38 @@
+package exercise1;
+
+public abstract class Insurance {
+
+	private final String typeOfInsurance; //Contains the type of insurance
+	protected double monthlyCost = 0.0; //Protected variable so its children have access to it
+
+    //Constructor
+	public Insurance(String typeOfInsurance, double monthlyCost){
+
+		if(monthlyCost <= 0.0) //Validation for monthly cost
+			throw new IllegalArgumentException("Monthly Cost must be greater than 0.0");
+			
+		//Assigning values
+		
+	    this.typeOfInsurance = typeOfInsurance;
+		this.monthlyCost = monthlyCost;
+		
+	}
+
+	//Getters
+	
+	public double getMonthlyCost() {
+
+		return monthlyCost;
+	}
+	
+	public String getTypeOfInsurance() {
+
+		return typeOfInsurance;
+	}
+	
+	//Abstract methods
+	
+	public abstract void setInsuranceCost(double newCost);
+	public abstract String displayInfo();
+
+}
