c09428a12410bf84934d4ce34aa3f09f97b3ef8c
Gaganbasra55 <38173348+Gaganbasra55@users.noreply.github.com>
2018-06-15 00:00:41 -0400

    completed with errors

--- src/exercise2/StudentDriver.java
+++ src/exercise2/StudentDriver.java
@@ -35,21 +35,45 @@ public class StudentDriver {
                 //Try and catch blocks are used for validations
                 System.out.println("Enter 1 to create a Full Time Student\n " +
                         "Enter 2 to create a Part Time Student\n");
-                        a = myScanner.next(); //Getting value
+                a = myScanner.next(); //Getting value
                 int option = Integer.parseInt(a); //parsing value
                 System.out.println();
 
 
-                    if (option == 1) {
-                        FullTimeStudent newStudent = new FullTimeStudent(name, true); //Creating full time student
-                        studentArray[i] = newStudent; //Populating array
-                        i++;
-                    }
-                    if (option == 2) {
+                if (option == 1) {
+                    FullTimeStudent newStudent = new FullTimeStudent(name, true); //Creating full time student
+                    studentArray[i] = newStudent; //Populating array
+                    i++;
+                }
+                if (option == 2) {
 
                     try {
 
                         System.out.println("Enter student's credit hours\n");
                         a = myScanner.next(); //Getting value
                         int creditHours = Integer.parseInt(a);
+                        try {
+
+
+                            PartTimeStudent newStudent = new PartTimeStudent(name, false, creditHours); //Creating part-time Student
+                            studentArray[i] = newStudent; //Populating array
+                            i++;
+
+                        } catch (IllegalArgumentException e) {
+                            System.out.println(e.getMessage());
+                        }
+
+                    } catch (Exception e) {
+                        System.out.println("You must enter a valid value for credit hours!\n");
+                    }
+                }
+                // display information
+
+                for(Student studentItem : studentArray){
+
+                    System.out.println(studentItem.displayInfo());
+                }
+                myScanner.close();
+            }
+
 
