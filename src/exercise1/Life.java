d19de04ebb4477ded7b07ca67565166e7a616dd7
Gaganbasra55 <38173348+Gaganbasra55@users.noreply.github.com>
2018-06-15 14:31:12 -0400

    life class created

--- src/exercise1/Life.java
+++ src/exercise1/Life.java
@@ -1,26 +1,22 @@
 package exercise1;
 
-public class Health extends Insurance {
+public class Life extends Insurance {
 
-	public Health(String typeOfInsurance, double monthlyCost) {
-
+	public Life(String typeOfInsurance, double monthlyCost) { // Constructor
 		super(typeOfInsurance, monthlyCost); // Super class constructor
-		
-		//this.monthlyCost =monthlyCost;
+
+		//this.monthlyCost = monthlyCost ;
 
 	}
 
-	//Overridden method setInsuranceMethod
-	
+
 	@Override
 	public void setInsuranceCost(double newCost) {
 
 		if (newCost <= 0.0)
 			throw new IllegalArgumentException("Monthly Cost must be greater than 0.0");
 
-		//Modifying protected variable monthlyCost
 		this.monthlyCost = newCost+monthlyCost;
-
 
 	}
 
@@ -29,8 +25,8 @@ public class Health extends Insurance {
 	@Override
 	public String displayInfo() {
 
-		return String.format("%s: %s%n%n%s: %.2f", "Insurance Type", super.getTypeOfInsurance(), "Monthly Cost",
-				super.getMonthlyCost());
+		return String.format("%s: %s%n%n%s: %.2f%n", "Insurance Type", super.getTypeOfInsurance(),
+				"Monthly Cost", super.getMonthlyCost());
 
 	}
 
