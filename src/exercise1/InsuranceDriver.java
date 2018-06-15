057a9f547bb77eee04c7ac97a6bffa8d0c589c2b
Gaganbasra55 <38173348+Gaganbasra55@users.noreply.github.com>
2018-06-15 15:04:40 -0400

    working on insurance driver class

--- src/exercise1/InsuranceDriver.java
+++ src/exercise1/InsuranceDriver.java
@@ -34,11 +34,20 @@ public class InsuranceDriver {
 						Health healthInsurance = new Health(typeOfInsurance, monthlyCost); //Creating health object
 						insuranceArray[control] = healthInsurance;
 						control++;
-					}
-					else
+					} else
 						JOptionPane.showMessageDialog(null, "Type of insurance unknown by the system! Try again");
 				}
 			} catch (IllegalArgumentException e) {
 				JOptionPane.showMessageDialog(null, e.getMessage(), "Error", 0);
 			}
 		}
+		try {
+			//Try and catch blocks are used to validate inputs
+
+			double newCost = Double
+					.parseDouble(JOptionPane.showInputDialog("Enter the new Monthly Cost for insurance " + (i + 1)
+							+ " of type: " + insuranceArray[i].getTypeOfInsurance().toUpperCase()));
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "You must enter a valid number for the new cost!", "Error", 0);
+		}
+	}
