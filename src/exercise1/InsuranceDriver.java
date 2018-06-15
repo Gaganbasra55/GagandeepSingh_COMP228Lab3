bbd80a97e25971d2c99689423e43cda0c27e88e1
Gaganbasra55 <38173348+Gaganbasra55@users.noreply.github.com>
2018-06-15 16:08:35 -0400

    completed exercise 1

--- src/exercise1/InsuranceDriver.java
+++ src/exercise1/InsuranceDriver.java
@@ -5,28 +5,27 @@ import javax.swing.JOptionPane;
 public class InsuranceDriver {
 
 	public static void main(String[] args) {
+        //Insurance array
+        Insurance[] insuranceArray = new Insurance[3];
 
-		Insurance[] insuranceArray = new Insurance[3]; //Insurance array
-		int control = 0; //Control variable
+        //Control variable
+        int control = 0;
 
-		//introduction
+        //introduction
 
-		JOptionPane.showMessageDialog(null,
-				"Creating " + insuranceArray.length + " objects of type Insurance!", "Welcome!", 1);
+        JOptionPane.showMessageDialog(null,
+                "Creating " + insuranceArray.length + " objects of type Insurance!", "Welcome!", 1);
 
-		//Loop through array to create objects
+        //Loop through array to create objects
 
-		while (control < insuranceArray.length) {
+        while (control < insuranceArray.length) {
 
-
-            //Try and catch blocks are used to validate inputs
             try {
-                //Getting values from end user
+                //Getting values from user
 
-                String typeOfInsurance = JOptionPane.showInputDialog("Enter the type of the insurance: Health or Life");
+                String typeOfInsurance = JOptionPane.showInputDialog("Enter the insurance type: Health or Life");
                 double monthlyCost = Double.parseDouble(JOptionPane.showInputDialog("Enter the Monthly Cost"));
 
-                //Try and catch blocks are used to validate inputs
                 try {
                     //Type of insurance Health
 
@@ -34,43 +33,64 @@ public class InsuranceDriver {
                         Health healthInsurance = new Health(typeOfInsurance, monthlyCost); //Creating health object
                         insuranceArray[control] = healthInsurance;
                         control++;
-                    } else {
+                    }
+                    else {
                         //Type of insurance Life
 
                         if (typeOfInsurance.toLowerCase().equals("life")) {
                             Life lifeInsurance = new Life(typeOfInsurance, monthlyCost); //Creating life object
                             insuranceArray[control] = lifeInsurance;
                             control++;
-                        } else
+                        }
+                        else
                             JOptionPane.showMessageDialog(null, "Type of insurance unknown by the system! Try again");
                     }
-                } catch (IllegalArgumentException e) {
+                }
+                catch (IllegalArgumentException e) {
                     JOptionPane.showMessageDialog(null, e.getMessage(), "Error", 0);
                 }
-            } catch (Exception e) {
+            }
+            catch (Exception e) {
                 JOptionPane.showMessageDialog(null, "You must enter a valid number!", "Error", 0);
             }
         }
-		try {
-			//Try and catch blocks are used to validate inputs
 
-			double newCost = Double
-					.parseDouble(JOptionPane.showInputDialog("Enter the new Monthly Cost for insurance " + (i + 1)
-							+ " of type: " + insuranceArray[i].getTypeOfInsurance().toUpperCase()));
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, "You must enter a valid number for the new cost!", "Error", 0);
-		}
-	}
-    //Loop through array to display information
+        //Custom messages
+        JOptionPane.showMessageDialog(null,
+                "Objects created! Now, you will set a new monthly cost for each object and they will be displayed in a polymorphic way",
+                "Information", 1);
 
-    int i = 1;
+        //Loop through array set new monthly cost
+        for (int i = 0; i < insuranceArray.length;) {
 
-		for(Insurance insuranceItem : insuranceArray){
+            try {
+                double newCost = Double.parseDouble(JOptionPane.showInputDialog("Enter the new Monthly Cost for insurance " + (i + 1)
+                        + " of type: " + insuranceArray[i].getTypeOfInsurance().toUpperCase()));
+                try {
+                     //Setting the new cost
+                    insuranceArray[i].setInsuranceCost(newCost);
+                    JOptionPane.showMessageDialog(null, "Monthly Cost successfully changed!", "Information", 1);										i++;
 
-        JOptionPane.showMessageDialog(null, "Insurance object: "+i+"\n"+insuranceItem.displayInfo(), "Display Information", 1);
-        i++;
+                }
+                catch (IllegalArgumentException e) {
+                    JOptionPane.showMessageDialog(null, e.getMessage(), "Error", 0);
+                }
+            }
+            catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "You must enter a valid number for the new cost!", "Error", 0);
+            }
+        }
+
+        //Loop through array to display information
+
+        int i = 1;
+
+        for(Insurance insuranceItem : insuranceArray){
+
+            JOptionPane.showMessageDialog(null, "Insurance object: "+i+"\n"+insuranceItem.displayInfo(), "Display Information", 1);
+            i++;
+        }
+
     }
-
-}
 
 }
