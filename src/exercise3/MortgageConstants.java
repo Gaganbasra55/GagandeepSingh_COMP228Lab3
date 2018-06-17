52c77f62fea178708faaf14acdbd2303d3d04c37
Gaganbasra55 <38173348+Gaganbasra55@users.noreply.github.com>
2018-06-17 11:22:51 -0400

    mortgage class

---
+++ src/exercise3/MortgageConstants.java
@@ -0,0 +1,14 @@
+package exercise3;
+
+public interface MortgageConstants {
+	
+	//Constants
+	
+	public final String bankName = "RBC Bank";
+	public final int maxMortgageAmount = 300000;
+	public final int shortTerm = 1;
+	public final int midTerm = 3;
+	public final int longTerm = 5;
+	
+	
+}
